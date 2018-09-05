27fec4af9598d601268f8e6c9e360bc05eab622b
Simon <gtrsimon@gmail.com>
2018-09-05 19:47:21 -0400

    Added shells

--- food-truck/src/app/app.module.ts
+++ food-truck/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
+import { CreateEventComponent } from './pages/create-event/create-event.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		CreateAccountComponent,
+		CreateEventComponent,
+		EditProfileComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
