440ab963423e83c0ed1336fcaefa4bc5f6372847
Simon <gtrsimon@gmail.com>
2018-09-06 20:13:01 -0400

    Added sass and basic styling

--- food-truck/src/app/app.component.ts
+++ food-truck/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { ComponentsModule } from './exports/components.module'
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	styleUrls: ['../sass/styles.scss', './app.component.scss']
 })
 export class AppComponent {
 	title = 'food-truck';
