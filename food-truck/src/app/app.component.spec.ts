844f68260c6d3c93ce4257c60cd35cb91d1d96a0
Simon <gtrsimon@gmail.com>
2018-09-11 20:02:50 -0400

    Fix App Component unit tests

--- food-truck/src/app/app.component.spec.ts
+++ food-truck/src/app/app.component.spec.ts
@@ -1,28 +1,35 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ComponentsModule } from './exports/components.module';
+import { ROUTES } from './exports/routes';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [
-				AppComponent
-			],
+			imports: [AppRoutingModule, ComponentsModule, RouterTestingModule.withRoutes(ROUTES)],
+			declarations: [AppComponent],
 		}).compileComponents();
 	}));
+
 	it('should create the app', async(() => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.debugElement.componentInstance;
 		expect(app).toBeTruthy();
 	}));
+
 	it(`should have as title 'food-truck'`, async(() => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.debugElement.componentInstance;
 		expect(app.title).toEqual('food-truck');
 	}));
+
 	it('should render title in a h1 tag', async(() => {
 		const fixture = TestBed.createComponent(AppComponent);
 		fixture.detectChanges();
 		const compiled = fixture.debugElement.nativeElement;
-		expect(compiled.querySelector('h1').textContent).toContain('Welcome to food-truck!');
+		expect(compiled.querySelector('h1').textContent).toContain('Food Truck Finder');
 	}));
 });
