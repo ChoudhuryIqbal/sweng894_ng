909934ac9835f7767f6b75dcfdedda279f8e6120
Simon <gtrsimon@gmail.com>
2018-09-16 16:03:36 -0400

    Routes export

--- food-truck/src/app/app-routing.module.ts
+++ food-truck/src/app/app-routing.module.ts
@@ -1,21 +1,9 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CreateAccountComponent } from './pages/create-account/create-account.component';
-import { EditProfileComponent } from "./pages/edit-profile/edit-profile.component";
-import { CreateEventComponent } from "./pages/create-event/create-event.component";
-import { LoginComponent } from "./pages/login/login.component";
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './exports/routes';
 
-
-const routes: Routes = [
-	{ path: 'createAccount', component: CreateAccountComponent },
-	{ path: 'editProfile', component: EditProfileComponent },
-	{ path: 'createEvent', component: CreateEventComponent},
-	{ path: 'login', component: LoginComponent },
-	{ path: '**', redirectTo: '/login', pathMatch: 'full' },
-
-];
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(ROUTES)],
 	exports: [RouterModule]
 })
 
