cd6781d001cd470e75b5092a3d45f58fe33176cd
Simon <gtrsimon@gmail.com>
2018-09-25 21:09:13 -0400

    Add HTTP Headers

--- food-truck/src/app/services/rest.service.ts
+++ food-truck/src/app/services/rest.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { IObject } from '../utils/interfaces';
@@ -7,10 +7,16 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class RestService {
 
+	httpOptions = {
+		headers: new HttpHeaders({
+			'Content-Type': 'application/json'
+		})
+	};
+
 	constructor(private http: HttpClient) { }
 
 	post(url: string, body: any): Observable<any> {
-		return this.http.post<any>(url, body)
+		return this.http.post<any>(url, body, this.httpOptions)
 			.pipe(catchError(this.handleError<any>('post', [])));
 	}
 
@@ -23,7 +29,7 @@ export class RestService {
 		return new Promise((resolve, reject) => {
 			this.get(url).subscribe(response => {
 				if (response > 300) {
-					subject.next({...response.body});
+					subject.next({ ...response.body });
 					resolve(response);
 				} else {
 					reject();
@@ -32,8 +38,21 @@ export class RestService {
 		});
 	}
 
-	private handleError<T> (operation='operation', result?: T){
-		return ( error : any): Observable<T> => {
+	promisePost(url: string, subject: BehaviorSubject<IObject>, payload: any): Promise<any> {
+		return new Promise((resolve, reject) => {
+			this.post(url, payload).subscribe(response => {
+				if (response > 300) {
+					subject.next({ ...response.body });
+					resolve(response);
+				} else {
+					reject();
+				}
+			});
+		});
+	}
+
+	private handleError<T>(operation = 'operation', result?: T) {
+		return (error: any): Observable<T> => {
 			console.error(error);
 			return of(result as T);
 		}
