adc272189e34abb66ddaa0edc7ea35dde5617506
Boredmp <gizmos46@verizon.net>
2018-10-05 19:59:33 -0400

    added comments

--- food-truck/src/app/pages/event-details/event-details.component.ts
+++ food-truck/src/app/pages/event-details/event-details.component.ts
@@ -4,6 +4,9 @@ import { EventService } from '../../services/event.service';
 import { Event } from '../../models/event';
 import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
 import { Review } from '../../models/review';
+import { Observable, forkJoin } from 'rxjs';
+import { switchMap } from 'rxjs/internal/operators/switchMap';
+import { switchMapTo, flatMap, concatMap, mergeMap, map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-event-details',
@@ -15,6 +18,7 @@ export class EventDetailsComponent implements OnInit {
     eventReviewForm: FormGroup;
     reviews : Array<Review>;
     pageReady = false;
+    avgRating = 0;
 
     ratings = [
         {rating: 5, text: ' - Excellent'},
@@ -26,33 +30,40 @@ export class EventDetailsComponent implements OnInit {
     constructor(private eventService: EventService, private route: ActivatedRoute, private fb: FormBuilder) { };
 
     ngOnInit() {
-        this.eventService.getEvent(+this.route.snapshot.params['id']).subscribe((event: Event) => {
-            this.event = event;
-            console.log(this.event);
-            this.eventReviewForm = this.buildFormGroup();
-            this.eventService.getReviews(this.event.vendorUsername).subscribe((reviews: Array<Review>) => {
+        const eventObservable: Observable<Event> = this.eventService.getEvent(+this.route.snapshot.params['id']);
+        const reviewsObservable: Observable<Array<Review>> = eventObservable.pipe(
+            switchMap((event: Event) => this.eventService.getReviews(event.vendorUsername))
+        );
+        
+        forkJoin(eventObservable, reviewsObservable).subscribe(
+            ([event, reviews]: [Event, Array<Review>]) => {
+                this.event = event;
                 this.reviews = reviews;
-            });
-        }); 
-          
+                this.eventReviewForm = this.buildFormGroup();
+                reviews.forEach(review => {
+                    this.avgRating += review.rating;
+                });
+                this.avgRating = this.avgRating/this.reviews.length;
+            }
+        );
     }
 
     onSubmit() {
         console.log(this.eventReviewForm.value);
-        const review = new Review(
-            this.event.vendorUsername, 
-            this.eventReviewForm.value.eventReview,
-            this.eventReviewForm.value.eventRating);
-        console.log(JSON.stringify(review));
-        this.eventService.createReview(JSON.stringify(review)).subscribe((review : Review) => {
-            this.reviews.push(review);
-        });
+        if (this.eventReviewForm.valid) {
+            this.eventService.createReview(JSON.stringify(this.eventReviewForm.value)).subscribe((review : Review) => {
+                this.reviews.push(this.eventReviewForm.value);
+                console.log(review);
+                this.eventReviewForm.reset();
+            });
+        }
     }
 
     private buildFormGroup(): FormGroup {
         return this.fb.group({
-            eventReview: ['', Validators.maxLength(140)],
-            eventRating: ['', Validators.required]
+            vendorUsername: [this.event.vendorUsername],
+            comment: ['', Validators.maxLength(140)],
+            rating: ['', Validators.required]
         });
     }
 }
