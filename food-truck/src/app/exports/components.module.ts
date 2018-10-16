9418356ee2683c9a3ac3887040936b77a05795ea
Simon <gtrsimon@gmail.com>
2018-10-16 19:52:46 -0400

    Removed unused edit profile component

--- food-truck/src/app/exports/components.module.ts
+++ food-truck/src/app/exports/components.module.ts
@@ -5,7 +5,6 @@ import { RouterModule } from '@angular/router';
 
 import { CreateAccountComponent } from "../pages/create-account/create-account.component";
 import { CreateEventComponent } from "../pages/create-event/create-event.component";
-import { EditProfileComponent } from "../pages/edit-profile/edit-profile.component";
 import { LoginComponent } from "../pages/login/login.component"
 import { EventsComponent } from '../pages/events/events.component';
 import { EventDetailsComponent } from '../pages/event-details/event-details.component';
@@ -14,7 +13,6 @@ import { EventsFilterPipe } from '../filters/eventsFilterPipe';
 const Components = [
     CreateAccountComponent,
     CreateEventComponent,
-    EditProfileComponent,
     LoginComponent,
     EventsComponent,
     EventDetailsComponent,
--- food-truck/src/app/exports/routes.ts
+++ food-truck/src/app/exports/routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from "@angular/router";
 import { CreateAccountComponent } from "../pages/create-account/create-account.component";
-import { EditProfileComponent } from "../pages/edit-profile/edit-profile.component";
 import { CreateEventComponent } from "../pages/create-event/create-event.component";
 import { LoginComponent } from "../pages/login/login.component";
 import { EventsComponent } from "../pages/events/events.component";
@@ -9,7 +8,6 @@ import { EventsResolverService } from "../services/events-resolver.service";
 
 export const ROUTES: Routes = [
 	{ path: 'createAccount', component: CreateAccountComponent },
-	{ path: 'editProfile', component: EditProfileComponent },
 	{ path: 'createEvent', component: CreateEventComponent },
 	{ path: 'login', component: LoginComponent },
 	{ path: '', redirectTo: '/login', pathMatch: 'full' },
